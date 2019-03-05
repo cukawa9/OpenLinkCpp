175bdf4111b89c897da9bb4a90c2e00fc86a013b
cukawa9 <maybewang9@gmail.com>
2019-03-05 17:28:56 +0800

    open link ok

--- WindowsCpp/OpenLink/OpenLink.cpp
+++ WindowsCpp/OpenLink/OpenLink.cpp
@@ -20,14 +20,52 @@ _declspec(dllexport) int subtract(int a, int b)
 	return a - b;
 }
 
+
+std::wstring s2ws(const std::string& s)
+{
+	int len;
+	int slength = (int)s.length() + 1;
+	len = MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, 0, 0);
+	wchar_t* buf = new wchar_t[len];
+	MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, buf, len);
+	std::wstring r(buf);
+	delete[] buf;
+	return r;
+}
+
+
 //system open the command line, need ignore it.
 _declspec(dllexport) void openlink(std::string myurl) {
 	system(std::string("start " + myurl).c_str());
 }
 
 _declspec(dllexport) void openlink_shell(std::string myurl) {
-	ShellExecute(NULL, L"open", LPCWSTR(myurl.c_str()), NULL, NULL, SW_SHOW);
+	//ShellExecute(NULL, L"open", LPCWSTR(myurl.c_str()), NULL, NULL, SW_SHOW);
+	std::wstring stemp = s2ws(myurl);
+	LPCWSTR address = stemp.c_str();
+
+	string open = "open";
+	std::wstring opentemp = s2ws(open);
+	LPCWSTR openlw = opentemp.c_str();
+
+	SHELLEXECUTEINFO ShExecInfo = { 0 };
+	ShExecInfo.cbSize = sizeof(SHELLEXECUTEINFO);
+	ShExecInfo.fMask = SEE_MASK_NOCLOSEPROCESS;
+	ShExecInfo.hwnd = NULL;
+	ShExecInfo.lpVerb = openlw;
+	ShExecInfo.lpFile = address;
+	cout << myurl.c_str() << endl;
+	ShExecInfo.lpParameters = (LPCWSTR)"";
+	ShExecInfo.lpDirectory = NULL;
+	ShExecInfo.nShow = SW_SHOW;
+	ShExecInfo.hInstApp = NULL;
+	ShellExecuteEx(&ShExecInfo);
+	WaitForSingleObject(ShExecInfo.hProcess, INFINITE);
+	LPDWORD lpExitCode = 0;
+	GetExitCodeProcess(ShExecInfo.hProcess, lpExitCode);
 }
+
+
 
 _declspec(dllexport) string readlink(string filename) {
 	CSimpleIni ini;
